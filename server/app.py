a1a507d6f1c59c5a1b3395edcb93afb51d7f3755
wilson mwangi waweru <kouragitam@gmail.com>
2024-10-03 11:45:48 +0300

    adds routes

--- server/app.py
+++ server/app.py
@@ -1,6 +1,5 @@
-from flask import Flask, make_response
+from flask import Flask, jsonify
 from flask_migrate import Migrate
-from sqlalchemy_serializer import SerializerMixin
 
 from models import db, Earthquake
 
@@ -19,14 +18,32 @@ def index():
     return make_response(body, 200)
 
 # Add views here
-@app.route('/earthquake')
-def list_eartquakes():
-    all_earthis = Earthquake.query.all()
-    Earthquake_list = [earthie.to_dict() for earthie in all_earthis]
-    resp_body = Earthquake_list
-    resp_code = 200
-    return make_response(resp_body, resp_code)
-    
+def get_earthquake(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake is None:
+        return jsonify({"message": f"Earthquake {id} not found."}), 404
+    return jsonify({
+        "id": earthquake.id,
+        "magnitude": earthquake.magnitude,
+        "location": earthquake.location,
+        "year": earthquake.year
+    })
+
+@app.route('/earthquakes/<int:id>')
+def earthquake(id):
+    return get_earthquake(id)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    if not earthquakes:
+        return jsonify({"count": 0, "quakes": []}), 200
+    return jsonify({"count": len(earthquakes), "quakes": [{
+        "id": earthquake.id,
+        "magnitude": earthquake.magnitude,
+        "location": earthquake.location,
+        "year": earthquake.year
+    } for earthquake in earthquakes]})
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
